0de7dfe1d6c6fd36043409b78638d72dfafad303
Thomas BECHET <thomas.bechet.pro@gmail.com>
2025-07-28 21:36:22 +0200

    adding ecs structures

---
+++ core/ecs/ecs.c
@@ -0,0 +1,104 @@
+#include "internal.h"
+
+static void
+ecs_mask_set (nux_ecs_mask_t *mask, nux_u32_t n)
+{
+    *mask = *mask | (1 << n);
+}
+static void
+ecs_mask_unset (nux_ecs_mask_t *mask, nux_u32_t n)
+{
+    *mask = *mask & ~(1 << n);
+}
+static nux_b32_t
+ecs_mask_isset (nux_ecs_mask_t mask, nux_u32_t n)
+{
+    return (mask >> n) & 1;
+}
+static nux_u32_t
+ecs_mask_count (nux_ecs_mask_t mask)
+{
+    nux_u32_t c;
+    for (c = 0; mask; mask >>= 1)
+    {
+        c += mask & 1;
+    }
+    return c;
+}
+static nux_b32_t
+ecs_bitset_isset (const nux_ecs_bitset_t *bitset, nux_u32_t index)
+{
+    nux_u32_t mask   = index / ECS_ENTITY_PER_MASK;
+    nux_u32_t offset = index % ECS_ENTITY_PER_MASK;
+    return (bitset->size > mask && ecs_mask_isset(bitset->data[mask], offset));
+}
+static nux_status_t
+ecs_bitset_set (nux_ecs_bitset_t *bitset, nux_u32_t index)
+{
+    nux_u32_t mask   = index / ECS_ENTITY_PER_MASK;
+    nux_u32_t offset = index % ECS_ENTITY_PER_MASK;
+    if (bitset->size <= mask)
+    {
+        if (mask >= bitset->capa)
+        {
+            return NUX_FAILURE; // max mask count reached
+        }
+        bitset->size = mask + 1;
+        // masks are already initialized to 0
+    }
+    ecs_mask_set(&bitset->data[mask], offset);
+    return NUX_SUCCESS;
+}
+static void
+ecs_bitset_unset (nux_ecs_bitset_t *bitset, nux_u32_t index)
+{
+    nux_u32_t mask   = index / ECS_ENTITY_PER_MASK;
+    nux_u32_t offset = index % ECS_ENTITY_PER_MASK;
+    if (bitset->size > mask)
+    {
+        ecs_mask_unset(&bitset->data[mask], offset);
+    }
+}
+static nux_ecs_mask_t
+ecs_bitset_mask (const nux_ecs_bitset_t *bitset, nux_u32_t mask_index)
+{
+    if (mask_index >= bitset->size)
+    {
+        return 0;
+    }
+    return bitset->data[mask_index];
+}
+static nux_u32_t
+ecs_bitset_count (const nux_ecs_bitset_t *bitset)
+{
+    nux_u32_t n = 0;
+    for (nux_u32_t i = 0; i < bitset->size; ++i)
+    {
+        n += ecs_mask_count(bitset->data[i]);
+    }
+    return n;
+}
+static nux_u32_t
+ecs_bitset_find_unset (const nux_ecs_bitset_t *bitset)
+{
+    for (nux_u32_t i = 0; i < bitset->size; ++i)
+    {
+        nux_ecs_mask_t mask = bitset->data[i];
+        if (bitset->data[i] != 0xFFFFFFFF)
+        {
+            nux_u32_t index = 0;
+            while (mask & 0x1)
+            {
+                mask >>= 1;
+                ++index;
+            }
+            return i * ECS_ENTITY_PER_MASK + index;
+        }
+    }
+    return bitset->size * ECS_ENTITY_PER_MASK;
+}
+static nux_u32_t
+ecs_bitset_capacity (const nux_ecs_bitset_t *bitset)
+{
+    return bitset->capa * ECS_ENTITY_PER_MASK;
+}
--- core/internal.h
+++ core/internal.h
@@ -733,6 +733,43 @@ typedef struct
     };
 } nux_disk_t;
 
+#define ECS_ENTITY_PER_MASK    32
+#define ECS_COMPONENT_NAME_LEN 64
+
+typedef nux_u32_t nux_ecs_mask_t;
+NUX_VEC_DEFINE(nux_ecs_bitset, nux_ecs_mask_t);
+NUX_VEC_DEFINE(nux_ecs_chunk_vec, void *);
+
+typedef struct
+{
+    nux_c8_t name[ECS_COMPONENT_NAME_LEN];
+} nux_ecs_component_t;
+
+typedef struct
+{
+    nux_u32_t           capa;
+    nux_ecs_chunk_vec_t chunks;
+    nux_ecs_bitset_t    bitset;
+    nux_u32_t           component_size;
+} nux_ecs_container_t;
+
+NUX_VEC_DEFINE(nux_ecs_container_vec, nux_ecs_container_t);
+
+typedef struct
+{
+    nux_u32_vec_t  includes;
+    nux_u32_vec_t  excludes;
+    nux_ecs_mask_t mask;
+    nux_u32_t      mask_index;
+    nux_u32_t      mask_offset;
+} nux_ecs_iter_t;
+
+typedef struct
+{
+    nux_ecs_bitset_t        bitset;
+    nux_ecs_container_vec_t containers;
+} nux_ecs_t;
+
 struct nux_context
 {
     // Thread data
@@ -802,6 +839,8 @@ struct nux_context
     // lua
 
     lua_State *L;
+
+    // ecs
 };
 
 ////////////////////////////
@@ -908,6 +947,29 @@ void  nux_scene_cleanup(nux_ctx_t *ctx, void *data);
 void *nux_scene_add_component(nux_ctx_t *ctx, nux_id_t node, nux_u32_t type);
 void  nux_scene_remove_component(nux_ctx_t *ctx, nux_id_t node, nux_u32_t type);
 void *nux_scene_get_component(nux_ctx_t *ctx, nux_id_t node, nux_u32_t type);
+
+// ecs.c
+
+nux_status_t nux_ecs_init(nux_ctx_t *ctx);
+nux_u32_t    nux_ecs_register_component(const nux_c8_t *name, nux_u32_t size);
+
+nux_id_t  nux_ecs_new(nux_u32_t capa);
+nux_u32_t nux_ecs_add(nux_id_t ecs);
+nux_u32_t nux_ecs_add_at(nux_id_t ecs, nux_u32_t index);
+void      nux_ecs_remove(nux_id_t ecs, nux_u32_t e);
+nux_b32_t nux_ecs_valid(nux_id_t ecs, nux_u32_t e);
+nux_u32_t nux_ecs_count(nux_id_t ecs);
+nux_u32_t nux_ecs_capacity(nux_id_t ecs);
+nux_u32_t nux_ecs_clear(nux_id_t ecs);
+void     *nux_ecs_set(nux_id_t ecs, nux_u32_t e, nux_u32_t c);
+void      nux_ecs_unset(nux_id_t ecs, nux_u32_t e, nux_u32_t c);
+nux_b32_t nux_ecs_has(nux_id_t ecs, nux_u32_t e, nux_u32_t c);
+void     *nux_ecs_get(nux_id_t ecs, nux_u32_t e, nux_u32_t c);
+nux_u32_t nux_ecs_iter(nux_u32_t include_count, nux_u32_t exclude_count);
+void      nux_ecs_includes(nux_u32_t iter, nux_u32_t c);
+void      nux_ecs_excludes(nux_u32_t iter, nux_u32_t c);
+nux_u32_t nux_ecs_begin(nux_id_t ecs, nux_u32_t iter);
+nux_u32_t nux_ecs_next(nux_id_t ecs, nux_u32_t iter);
 
 // vector.c
 
