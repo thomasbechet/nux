6bfca77115367f5f1989028a8b8882f140746c50
Thomas BECHET <thomas.bechet.pro@gmail.com>
2025-09-12 07:51:52 +0200

    fix build script

--- examples/simple/build.lua
+++ examples/simple/build.lua
@@ -1,7 +1,19 @@
-function nux.init()
-    assert(nux.io.cart_begin("cart.bin", 3))
-    assert(nux.io.write_cart_file("init.lua"))
-    assert(nux.io.write_cart_file("inspect.lua"))
-    assert(nux.io.write_cart_file("assets/industrial.glb"))
+local mod = MODULE
+
+function mod:on_load()
+    local files = {
+        "conf.lua",
+        "init.lua",
+        "camera.lua",
+        "camera.lua",
+        "libs/inspect.lua",
+        "assets/industrial.glb"
+    }
+    assert(nux.io.cart_begin("cart.bin", #files))
+    for _, v in pairs(files) do
+        assert(nux.io.write_cart_file(v))
+    end
     assert(nux.io.cart_end())
 end
+
+return mod
